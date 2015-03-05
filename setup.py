69690e0df0529de073899a5c8f5a11ef2879aad3
Roberto Menezes <robertomenezes@MacBook-de-Roberto.local>
2015-03-04 21:50:35 -0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Synced: ES & Cassandra as one',
+    'author': 'Roberto Weidmann Menezes',
+    'url': 'URL to get it at.',
+    'download_url': 'Where to download it.',
+    'author_email': 'robertowm (at) gmail (dot) com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['synced'],
+    'scripts': [],
+    'name': 'synced'
+}
+
+setup(**config)
