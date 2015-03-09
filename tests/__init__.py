7b9ffbaa2d5c9f3ac599274c5980457fbfb97e50
Roberto Menezes <robertomenezes@MacBook-de-Roberto.local>
2015-03-08 23:32:28 -0300

    Some 'tests', very simple. (Forgot to commit early)'

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,9 +6,11 @@ import time_uuid
 ##refact tests
 
 def testCassandra():
+    print 'Cassandra: Begin'
     conn = CassandraConnector('demo')
     for table in conn.list_tables():
         print 'Table:', table
+        """
         for row in conn.load(table, datetime.utcfromtimestamp(0), datetime(2020, 1, 1)):
             print '\t', row
             row['name'] += '.'
@@ -17,16 +19,25 @@ def testCassandra():
         print '---'
         for row in conn.load(table, datetime.utcfromtimestamp(0), datetime(2020, 1, 1)):
             print '\t', row
+        """
     conn.close()
+    print 'Cassandra: End'
 
 testCassandra()
 
 def testES():
+    print 'ES: Begin'
     conn = ESConnector('demo')
     date = time_uuid.TimeUUID.convert(datetime(2015, 2, 1))
-    conn.upsert('users', {'name': 'Mari...', 'id': 2, 'tmstmp': date})
-    for row in conn.load(datetime.utcfromtimestamp(0), datetime(2020, 1, 1)):
-        print '\t', row
-    conn.close()
+    for table in conn.list_tables():
+        print 'Table:', table
+
+        """
+        conn.upsert(table, {'name': 'Mari...', 'id': 2, 'tmstmp': date})
+        for row in conn.load('users', datetime.utcfromtimestamp(0), datetime(2020, 1, 1)):
+            print '\t', row
+        conn.close()
+        """
+    print 'ES: End'
 
 testES()
